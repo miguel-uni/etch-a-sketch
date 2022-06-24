6babbdcf38921bdee6d887055b4945cf3b3099e8
Miguel <macgarcia2002@gmail.com>
2022-06-24 01:12:31 -0400

    Create functions to make divs where squares are, Create function to paint squares, Create function to change quantity of squares

--- js/script.js
+++ js/script.js
@@ -1,28 +1,67 @@
-function createParentDivs() {
-    for (let i = 1; i <= 16; i++) {
-        const container = document.getElementById('container');
-        const div = document.createElement('div');
+function createParentDivs(num) {
+    for (let i = 1; i <= num; i++) {
+        const gridContainer = document.getElementById('grid-container');
+        const parentDiv = document.createElement('div');
 
-        div.setAttribute('id', i);
-        div.classList.add('parent');
-        container.appendChild(div);
+        parentDiv.setAttribute('id', i);
+        parentDiv.classList.add('square-container');
+        gridContainer.appendChild(parentDiv);
     };
 };
 
-function createChildDivs() {
-    for (let i = 1; i <= 16; i++) {
+function createChildDivs(num) {
+    for (let i = 1; i <= num; i++) {
         const parentDiv = document.getElementById(i);
         
         if (parentDiv) {
-            for (let z = 1; z <= 16; z++) {
-                const div = document.createElement('div');
+            for (let z = 1; z <= num; z++) {
+                const childDiv = document.createElement('div');
                 
-                div.classList.add('child');
-                parentDiv.appendChild(div);
+                childDiv.setAttribute('id', 'child-div');
+                childDiv.classList.add('square');
+                parentDiv.appendChild(childDiv);
             };
         };
     };
 };
 
-createParentDivs();
-createChildDivs();
+function deleteDivs(cssSelector) {
+    let nodes = document.querySelectorAll(cssSelector);
+
+    for(let i = 0; i < nodes.length; i++){
+        nodes[i].parentNode.removeChild(nodes[i]);
+    };
+};
+
+function paintSquares() {
+    let allSquares = document.querySelectorAll(".square");
+    
+    allSquares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.classList.add('active');
+        });
+    });
+};
+
+function changeQuantityOfSquares() {
+    const button = document.getElementById('squares-selection');
+    
+    button.addEventListener('click', () => {
+        let num = prompt('how many squares do you want?');
+    
+        if (num <= 100) {
+            deleteDivs('#child-div');
+            deleteDivs('square-container');
+            createParentDivs(num);
+            createChildDivs(num);
+            paintSquares();
+        } else {
+            return;
+        };
+    });
+};
+
+createParentDivs(16);
+createChildDivs(16);
+paintSquares();
+changeQuantityOfSquares();
